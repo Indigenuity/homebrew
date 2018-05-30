d28c8036bccab739bfea9f435d23946f6e19ad33
JD <Indigenuity@users.noreply.github.com>
2018-05-30 10:06:29 -0400

    Update jq.rb

--- Formula/jq.rb
+++ Formula/jq.rb
@@ -1,12 +1,12 @@
 class Jq < Formula
   desc "Lightweight and flexible command-line JSON processor"
   homepage "https://stedolan.github.io/jq/"
-  root_url “https://homebrew.bintray.com/bottles”
   url "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz"
   sha256 "c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c"
   revision 3
 
   bottle do
+    root_url “https://homebrew.bintray.com/bottles”
     cellar :any
     sha256 "25be689b9bca3cef2ee0cb647388200d25f045f651679ef8871b8a86100f9e43" => :high_sierra
     sha256 "11e169f340dc1f93fbb3c21a87c3aaa7d1242967ed11672663e6e827e622ef0d" => :sierra
